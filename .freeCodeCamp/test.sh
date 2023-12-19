da59b2f8aa85dd221848dcb24b5df5a52468699d
yunus99-22 <ramadhanyunus17@gmail.com>
2023-12-19 07:21:37 -0500

    first commit

---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Program to tell a persons fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+}
+$GET_FORTUNE
+read QUESTION
+
+
+echo $QUESTION
